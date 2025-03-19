43d8dc250b6471444987e36d354916e86dfcef4b
smirnov-vv <vsmirnov.pro@gmail.com>
2025-03-19 12:35:43 +0300

    fix: allow HTTP/1.1 fallback

--- src/server.js
+++ src/server.js
@@ -6,16 +6,20 @@ import fastifyStatic from '@fastify/static';
 import fastifyView from '@fastify/view';
 import pug from 'pug';
 import userRoutes from '../server/routes/users.js';
+import searchRoutes from '../server/routes/search.js';
+import requestRoutes from '../server/routes/requests.js';
 import offlineRoute from '../server/routes/offline.js';
 import dotenv from 'dotenv';
 import fastifyFormbody from '@fastify/formbody';
+import fastifyCookie from '@fastify/cookie';
 
-dotenv.config();
+dotenv.config(); // Load environment variables
 
 // HTTPS options
 const httpsOptions = {
   key: fs.readFileSync(path.join(process.cwd(), 'sslcert', 'privkey.pem')),
   cert: fs.readFileSync(path.join(process.cwd(), 'sslcert', 'fullchain.pem')),
+  allowHTTP1: true, // This is useful if some clients do not support HTTP/2
 };
 
 // Initialize Fastify with HTTPS
@@ -24,6 +28,9 @@ const app = fastify({
   http2: true,
   https: httpsOptions,
 });
+
+// Register the cookie plugin
+app.register(fastifyCookie);
 
 // Register the Fastify body parser (built-in for application/x-www-form-urlencoded)
 app.register(fastifyFormbody);
@@ -40,31 +47,39 @@ app.register(fastifyView, {
 });
 
 app.register(userRoutes, { prefix: '/api' });
+app.register(searchRoutes, { prefix: '/api' });
+app.register(requestRoutes, { prefix: '/api' });
 app.register(offlineRoute, { prefix: '/api' });
 
 // Custom error handler
 app.setErrorHandler((error, request, reply) => {
+  const isLoggedIn = request.cookies?.token ? true : false;
   if (error.validation) {
     // Handle validation errors
     return reply.status(400).view('index.pug', {
-      statusCode: 400,
-      message: error.message.split('/')[1],
-      tryAgain: 'Try again',
-      details: error,
+      title: error.message.split('/')[1],
+      message: 'Try again',
+      isLoggedIn,
     });
   }
 
   // Handle other errors
+  console.error('setErrorHandler error:', error);
   reply.status(500).view('index.pug', {
-    statusCode: 500,
+    title: 'setErrorHandler error',
     message: 'Internal Server Error',
-    details: error.message,
+    isLoggedIn,
   });
 });
 
-// Example route
+// Starting route
 app.get('/', async (request, reply) => {
-  return reply.view('index.pug', { message: 'Let\'s get started' });
+  const isLoggedIn = request.cookies?.token ? true : false;
+  return reply.view('index.pug', {
+    title: "Let's get started!",
+    message: '',
+    isLoggedIn,
+  });
 });
 
 // Start the server
@@ -73,7 +88,7 @@ const start = async () => {
     await app.listen({ port: 3000, host: '0.0.0.0' }); // In Docker, the host should be 0.0.0.0 to allow external access.
     console.log('Server listening on https://localhost:3000');
   } catch (err) {
-    console.error(err);
+    console.error(`start the server error: ${err}`);
     process.exit(1);
   }
 };
